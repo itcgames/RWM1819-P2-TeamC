215371573ace4fb76051301b0f4c36a17f34793c
zjacques <zachary.jacques160@gmail.com>
2018-11-28 21:29:31 +0000

    I forgot what I did and it's too late to figure it out

--- src/gameScene.js
+++ src/gameScene.js
@@ -14,14 +14,24 @@ class gameScene extends Scene{
 
         this.pauseDiv =  document.createElement('div');
         this.pauseDiv.id = "pause screen";
-        this.pauseDiv.position = "relative";
+        this.pauseDiv.style.position = "absolute";
     
         this.pauseDiv.style.width = '100%';
         this.pauseDiv.style.height = '100%';
-        this.pauseDiv.style.background = "rgba(155,155,155,155)"
+        this.pauseDiv.style.zIndex = '2';
+        this.pauseDiv.style.backgroundColor = "rgba(155, 155, 155, 0.5)";
         this.pauseDiv.style.display = 'none';
         this._containerDiv.appendChild(this.pauseDiv);
         this._containerDiv.appendChild(this.statusBar);
+
+        let stat = this.statusBar;
+        let pause = this.pauseDiv;
+        new Button("Pause Button", stat, function(){
+            pause.style.display = 'block';
+        }, {'x': 2, 'y': 40, 'width': 10, 'height': 10}, "%");
+        new Button("Unpause Button", pause, function(){
+            pause.style.display = 'none';
+        }, {'x': 50, 'y': 50, 'width': 10, 'height': 10}, "%");
 
         this.init();
 
@@ -36,6 +46,26 @@ class gameScene extends Scene{
         canv.style.height = "90%";
         canv.style.width = '100%';
         canv.style.top = "10%";
+    }
+
+    resizeCanvas(){
+        let canv =  document.getElementById("canvas");
+        let screenX = document.body.clientWidth;
+        let screenY = document.body.scrollHeight*0.9;
+        let ratio = 19/9;
+        let newRatio = screenX/screenY;
+
+        if (newRatio > ratio) {
+            screenX = screenY * widthToHeight;
+            canv.style.height = screenY + 'px';
+            canv.style.width = screenX + 'px';
+        } else {
+            newHeight = screenX / widthToHeight;
+            canv.style.width = screenX + 'px';
+            canv.style.height = screenY + 'px';
+        }
+
+
     }
 
 
